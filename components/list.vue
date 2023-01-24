6fa64c1c248ead2e91fbc9e84fcda404345e7b4f
Antoine Guglielmi <antoine.guglielmi@insign.fr>
2023-01-24 10:28:21 +0100

    Create list.vue component

---
+++ components/list.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+const props = withDefault(defineProps<{
+	items: Array<unknown>,
+	nick?: string
+}>(), {
+	nick: 'item'
+});
+</script>
+
+<template>
+	<ul>
+		<li
+			v-for="(item, index) in items"
+		>
+			<slot :name="nick" v-bind="{ [nick]: item, index }"></slot>
+		</li>
+	</ul>
+</template>
